e155a58bea2485228ea3278f81a2cb6e11e693f6
Marc White <marc.white@anu.edu.au>
2020-10-11 21:31:57 +1100

    Re-structure models & remove migrations history

--- scoresheet_generator/admin.py
+++ scoresheet_generator/admin.py
@@ -6,6 +6,18 @@ from . import models
 
 # Register your models here.
 
-admin.site.register(models.Team)
+# admin.site.register(models.Team)
 admin.site.register(models.Competition)
+admin.site.register(models.Division)
 admin.site.register(models.Venue)
+
+
+class PlayerInline(admin.TabularInline):
+    model = models.Player
+
+
+class TeamAdmin(admin.ModelAdmin):
+    inlines = [
+        PlayerInline,
+    ]
+admin.site.register(models.Team, TeamAdmin)
--- scoresheet_generator/models.py
+++ scoresheet_generator/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -12,21 +13,47 @@ DIVISIONS = (
     ('2', 'Division 2'),
 )
 
+ROLES = (
+    ('C', 'Captain'),
+    ('G', 'Goalkeeper'),
+    ('S', 'Team staff'),
+)
+
 
 class Team(models.Model):
 
     class Meta:
-        unique_together = ('team_name', 'division', )
+        unique_together = ('team_name', )
 
     # Model fields
     team_name = models.CharField(max_length=100, blank=False,
                                  unique=False)
-    division = models.CharField(max_length=2, blank=False,
-                                choices=DIVISIONS)
     team_list = models.FileField(blank=True)
 
     def __unicode__(self):
-        return '{} ({})'.format(self.team_name, self.division.upper())
+        return '{}'.format(self.team_name, )
+
+
+class Player(models.Model):
+
+    class Meta:
+        unique_together = ('team', 'number', )
+
+    # Model fields
+    name = models.CharField(max_length=300, blank=False, unique=False)
+    number = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(99)],
+        blank=True
+    )
+    role = models.CharField(max_length=1, choices=ROLES, blank=True)
+    team = models.ForeignKey(Team)
+
+    def __unicode__(self):
+        return '#{}. {} ({})'.format(
+            self.number,
+            self.name,
+            self.team.team_name,
+        )
 
 
 class Competition(models.Model):
@@ -40,6 +67,21 @@ class Competition(models.Model):
         return '{}{}'.format(
             self.comp,
             ' ({})'.format(self.assoc) if self.assoc else ''
+        )
+
+
+class Division(models.Model):
+    class Meta:
+        unique_together = ('comp', 'div')
+
+    comp = models.ForeignKey(Competition)
+    div = models.CharField(max_length=1, choices=DIVISIONS)
+    teams = models.ManyToManyField(Team, blank=True)
+    active = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return '{} {}'.format(
+            self.comp, self.get_div_display(),
         )
 
 
