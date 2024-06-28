5088989f74f558627fa56e4738175784ba6a1755
Marc White <marc.white@anu.edu.au>
2024-06-28 22:59:04 +1000

    Correctly build combined PDF and XSLX output (but images not duplicated on XSLX worksheets)

--- scoresheet_generator/views.py
+++ scoresheet_generator/views.py
@@ -14,6 +14,7 @@ from django.shortcuts import redirect
 from django.conf import settings
 
 from pdfrw import PdfWriter, IndirectPdfDict, PdfReader, PdfObject, PdfDict
+import openpyxl
 
 from django.conf import settings
 
@@ -42,7 +43,9 @@ def generate(request):
 
         ss_formset = ss_formset(request.POST)
         if ss_formset.is_valid():
-            pdfs = []
+            sheets = []
+            start_date = None
+            end_date = None
             for form in ss_formset:
                 if form.is_valid() and form.has_changed():
                     template = writer_func(
@@ -55,34 +58,87 @@ def generate(request):
                         duty_team=form.cleaned_data.get('duty_team'),
                         venue=form.cleaned_data.get('venue'),
                     )
-                    p = os.path.join(
-                        settings.MEDIA_ROOT,
-                        '{}{}_{}-v-{}.{}'.format(
-                            form.cleaned_data.get('start_time').strftime(
-                                '%Y-%m-%d_%H%M'),
-                            '_{}'.format(form.cleaned_data.get('match_id')) if
-                            form.cleaned_data.get('match_id') else
-                            '',
-                            form.cleaned_data.get('home_team').team_name[:4],
-                            form.cleaned_data.get('away_team').team_name[:4],
-                            fmt
-                        ))
-                    print(f"Created filename {p}")
-                    
-                    if fmt == "pdf":
-                        PdfWriter().write(p, template)
-                    elif fmt == "xlsx":
-                        template.save(p)
+                    if start_date is None or start_date > form.cleaned_data.get('start_time'):
+                        start_date = form.cleaned_data.get('start_time')
+                    if end_date is None or end_date < form.cleaned_data.get('start_time'):
+                        end_date = form.cleaned_data.get('start_time')
+                    # p = os.path.join(
+                    #     settings.MEDIA_ROOT,
+                    #     '{}{}_{}-v-{}.{}'.format(
+                    #         form.cleaned_data.get('start_time').strftime(
+                    #             '%Y-%m-%d_%H%M'),
+                    #         '_{}'.format(form.cleaned_data.get('match_id')) if
+                    #         form.cleaned_data.get('match_id') else
+                    #         '',
+                    #         form.cleaned_data.get('home_team').team_name[:4],
+                    #         form.cleaned_data.get('away_team').team_name[:4],
+                    #         fmt
+                    #     ))
+                    # print(f"Created filename {p}")
 
-                    pdfs.append(p)
+                    sheets.append(template)
+
+            # Merge the individual sheets into a master file
+            # (easier file handling, and uses less API hits for 
+            #  converting XSLX->PDF via Adobe APIs)
+            if start_date is None or end_date is None:
+                output_fn = f"sheets_generated_at_{datetime.datetime.now().strftime('%Y-%m-%d-%H%M')}.{fmt}"
+            elif start_date.date() == end_date.date():
+                output_fn = f"sheets_{start_date.strftime('%Y-%m-%d')}.{fmt}"
+            else:
+                output_fn = f"sheets_{start_date.strftime('%Y-%m-%d')}-TO-{end_date.strftime('%Y-%m-%d')}.{fmt}"
+            output_fn_full = os.path.join(settings.MEDIA_ROOT, output_fn)
+
+            if fmt == "pdf":
+                import pdb; pdb.set_trace()
+                output_pdf = PdfWriter()
+                for sheet in sheets:
+                    for page in sheet.pages:
+                        output_pdf.addpage(page)
+                output_pdf.write(output_fn_full)
+                print("Attempting redirect to %s", '/'.join(output_fn_full.split(os.sep)[-2:]))
+            
+            if fmt == "xlsx":
+                output_wb = openpyxl.reader.excel.load_workbook(template_path)
+                sheet_names_orig = output_wb.get_sheet_names()
+                # Make duplicate match record sheets as required
+                for i in range(1, len(sheets)):
+                    for sheet_name in sheet_names_orig:
+                        new_sheet = output_wb.copy_worksheet(output_wb[sheet_name])
+                        new_sheet.title = f"{sheet_name}{i+1}"
+
+                # Iterate over the match records in memory, and copy them
+                # in to the output workbook
+                for i, sheet in enumerate(sheets):
+                    for sheet_name in sheet_names_orig:
+                        filled_sheet = sheet[sheet_name]
+                        blank_sheet = output_wb[
+                            f"{sheet_name}{i+1}" if i != 0 else sheet_name
+                            ]
+                        mr = filled_sheet.max_row
+                        mc = filled_sheet.max_column
+
+                        for r in range(1, mr+1):
+                            for c in range(1, mc+1):
+                                try:
+                                    blank_sheet.cell(
+                                        row=r, column=c
+                                        ).value = filled_sheet.cell(row=r, column=c).value
+                                except AttributeError:  # Merged cell, ignore
+                                    pass
+                
+                output_wb.save(output_fn_full)
+
+            return redirect('/'.join(output_fn_full.split(os.sep)[-2:]))
+
 
             # Make and serve a zip of these
-            zip_name = 'sheets_made_at_' + datetime.datetime.now().strftime('%Y-%m-%d-%H%M') + '.zip'
-            print(f"Preparing to zip to {zip_name}")
-            zip_path = os.path.join(settings.MEDIA_ROOT, zip_name)
-            subprocess.check_call(['zip', '-jFS', zip_path] + pdfs)
-            print("Zip complete!")
-            return redirect('/'.join(zip_path.split(os.sep)[-2:]))
+            # zip_name = 'sheets_made_at_' + datetime.datetime.now().strftime('%Y-%m-%d-%H%M') + '.zip'
+            # print(f"Preparing to zip to {zip_name}")
+            # zip_path = os.path.join(settings.MEDIA_ROOT, zip_name)
+            # subprocess.check_call(['zip', '-jFS', zip_path] + pdfs)
+            # print("Zip complete!")
+            # return redirect('/'.join(zip_path.split(os.sep)[-2:]))
 
     return render(request, 'scoresheet_generator/generator.html',
                   {
